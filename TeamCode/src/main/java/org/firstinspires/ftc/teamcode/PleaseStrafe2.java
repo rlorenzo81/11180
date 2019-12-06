ce7e63da62e86dbea945bd9edc123936f6f4fa65
Robert Lorenzo <rlorenzo@essextech.org>
2019-12-06 09:10:44 -0500

    Update PleaseStrafe2.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PleaseStrafe2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PleaseStrafe2.java
@@ -32,6 +32,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
@@ -97,6 +98,7 @@ import static org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocaliz
 @Autonomous(name="Please Strafe 2", group="Pushbot")
 //
 // @Disabled
+@Disabled
 public class PleaseStrafe2 extends LinearOpMode {
 
     /* Declare OpMode members. */
