2013db5308204805d1838e97fd05ae6d0e57fe15
Robert Lorenzo <rlorenzo@essextech.org>
2019-12-06 12:38:42 -0500

    Update AutoBlueDepotSideStock.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoBlueDepotSideStock.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoBlueDepotSideStock.java
@@ -32,6 +32,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
@@ -96,7 +97,9 @@ import static org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocaliz
 
 @Autonomous(name="AutoBlueDepotStock", group="Pushbot")
 //
-// @Disabled
+//@Disabled
+@Disabled
+
 public class AutoBlueDepotSideStock extends LinearOpMode {
 
     /* Declare OpMode members. */
